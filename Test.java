449176f0da0f295df25e6e86cd0e81ce421caef6
Mowei <mowei930425@gmail.com>
2024-11-05 14:17:45 +0800

    master branch version

--- Test.java
+++ Test.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
-
 public class Test {
     public static void main(String[] args) {
-        System.out.println("這是Git的練習用程式，是個加法器，請輸入一個數字，答案會是輸入+10");
-        Scanner input = new Scanner(System.in);
-        int x, y;
-        x = input.nextInt();
-        y = 10;
-        System.out.println("x + y = " + (x + y));
+        System.out.println("這是Git的練習用程式");
+        System.out.println("這是master branch的版本");
     }
 }
